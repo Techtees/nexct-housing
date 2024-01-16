caad8df363a356d566b59e3ff1bde8392ca88e9c
taiwoj121 <taiwoj121@gmail.com>
2024-01-16 13:33:02 +0100

    page routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import SignIn from './pages/SignIn'
+import Offers from './pages/Offers'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import SignUp from './pages/Signup'
 
 function App() {
   return (
     <div className="App">
-      <h1>lolde</h1>
+     <Router>
+        <Routes> 
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+     </Router>
     </div>
   );
 }
