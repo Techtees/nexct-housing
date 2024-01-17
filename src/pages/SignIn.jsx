3588da101e17bafe5cb8e611c437b070708efe33
taiwoj121 <taiwoj121@gmail.com>
2024-01-17 17:47:27 +0100

    user sign-in  logic implemented

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 function SignIn() {
@@ -20,6 +21,22 @@ function SignIn() {
 
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+       try{
+            const auth = getAuth()
+
+            const userCredential = await signInWithEmailAndPassword(auth, email , password)
+
+            if(userCredential.user) {
+                navigate('/')
+            }
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     const navigate = useNavigate()
 
     return ( 
@@ -28,7 +45,7 @@ function SignIn() {
                 <header>
                     <p className="pageHeader">Welcome Back!</p>
 
-                    <form action="">
+                    <form onSubmit={handleSubmit}>
                         <input 
                             type="email"  placeholder="Email"
                             className="emailInput"
