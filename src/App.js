a43a13f6fd1e404d799cbd9076965fe765e9bcaa
taiwoj121 <taiwoj121@gmail.com>
2024-01-18 18:48:08 +0100

    react toastify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import SignIn from './pages/SignIn'
@@ -17,11 +19,12 @@ function App() {
           <Route path='/offers' element={<Offers />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/profile' element={<SignIn />} />
+          <Route path='/profile' element={<Profile/>} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
      </Router>
+     <ToastContainer />
     </div>
   );
 }
